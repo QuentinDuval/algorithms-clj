7270be332cc0e31e3b9f29c350bd1868b1771a65
QuentinDuval <senmenty@gmail.com>
2016-09-11 16:06:05 +0200

    Integrating the different testing means

--- test/clj/algorithms_clj/huffman_test.clj
+++ test/clj/algorithms_clj/huffman_test.clj
@@ -3,40 +3,35 @@
     [algorithms-clj.huffman :refer :all]
     [clojure.test :refer :all]
     [clojure.test.check :as tc]
+    [clojure.test.check.clojure-test :refer [defspec]]
     [clojure.test.check.generators :as gen]
     [clojure.test.check.properties :as prop]
+    [midje.sweet :refer :all]
     ))
 
 
 ;; -------------------------------------------------------
 
-(defn- to-encoding-map
+(defn to-encoding-map
   [value-frequency-pairs]
   (huffman-tree->encoding-map (make-huffman-tree value-frequency-pairs)))
 
-(deftest make-huffman-tree-test
-  "Tests on the encoding of the huffman tree"
-  
-  (testing "Empty input leads to empty huffman tree"
-    (is (= nil (make-huffman-tree [])))
-    (is (= {} (to-encoding-map []))))
-  
-  (testing "Single generate a one symbol alphabet"
-    (let [inputs [["a" 1]]]
-      (is (= {"a" [0]} (to-encoding-map inputs)))
-      ))
-  
-  (testing "Two inputs generate two one digit symbols"
-    (let [inputs [["a" 1] ["b" 2]]]
-      (is (= {"a" [0] "b" [1]} (to-encoding-map inputs)))
-      ))
-   
-  (testing "three inputs - coding map"
-    (let [inputs [["a" 1] ["b" 4] ["c" 2]]
-          expected {"a" [0 0], "b" [1], "c" [0 1]}]
-      (is (= expected (to-encoding-map inputs)))
-      ))
-  )
+(deftest huffman-code-generation
+  (facts "About the generation of huffman encoding map"
+    (fact "No inputs leads to a nil tree and no encoding"
+      (make-huffman-tree []) => nil
+      (to-encoding-map []) => {}
+      )
+    (fact "Signal with no information still generates a encoding map"
+      (to-encoding-map [["a" 1]]) => {"a" [0]}
+      )
+    (fact "Two symbols always leads to a map with single digit codes"
+      (to-encoding-map [["a" 1] ["b" 2]]) => {"a" [0] "b" [1]}
+      )
+    (fact "With 3 inputs, the most common symbol has the least number of digit"
+      (to-encoding-map [["a" 1] ["b" 4] ["c" 2]]) => {"a" [0 0], "b" [1], "c" [0 1]}
+      )
+    ))
 
 ;; -------------------------------------------------------
 
@@ -44,8 +39,10 @@
   (prop/for-all [original (gen/vector gen/int 1 100)]
     (let [[code encoded] (encode original)
           decoded (decode code encoded)]
-      (= original decoded))
-    ))
+      (= original decoded))))
 
-(tc/quick-check 100 decode-code-cycle)
+(defspec decoding-encoded-message-should-return-the-orginal
+  100 ;; Will call (tc/quick-check 100 decode-code-cycle)
+  decode-code-cycle)
+
 (run-tests)
