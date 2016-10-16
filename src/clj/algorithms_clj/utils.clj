b3c92c2bad9ec2543c0051d82bdb3370f6bf9af4
Duval Quentin <duvalquentin@macbook-pro-de-duval.home>
2016-10-16 20:07:55 +0200

    Refactoring of the huffman algorithm to use a macro that helps creating a transducer

--- src/clj/algorithms_clj/utils.clj
+++ src/clj/algorithms_clj/utils.clj
@@ -2,29 +2,42 @@
 
 
 (defn map-first
-  ([f] (map (fn [[a b]] [(f a) b])))
-  ([f c] (eduction (map-first f) c)))
+      ([f] (map (fn [[a b]] [(f a) b])))
+      ([f c] (eduction (map-first f) c)))
 
 (defn iterate-until-single-value
-  "Apply a reducing step on a collection until its size is equal to 1, and return that element"
-  [step collection]
-  (loop [result collection]
-    (if (< 1 (count result))
-      (recur (step result))
-      (first result))
-    ))
+      "Apply a reducing step on a collection until its size is equal to 1, and return that element"
+      [step collection]
+      (loop [result collection]
+            (if (< 1 (count result))
+              (recur (step result))
+              (first result))
+            ))
 
 (defmacro reduce-to-single-value
-  [[collection] & body]
-  `(iterate-until-single-value
-     (fn [~collection] ~@body) ~collection))
+          [[collection] & body]
+          `(iterate-until-single-value
+             (fn [~collection] ~@body) ~collection))
 
 (defn rotations
-  "Generates all the rotations of an input sequence"
-  [inputs]
-  (let [n (count inputs)
-        xf (comp (map #(take n %)) (take n))]
-    (eduction xf
-      (iterate #(drop 1 %) (concat inputs inputs)))
-    ))
+      "Generates all the rotations of an input sequence"
+      [inputs]
+      (let [n (count inputs)
+            xf (comp (map #(take n %)) (take n))]
+           (eduction
+             xf
+             (iterate #(drop 1 %) (concat inputs inputs)))
+           ))
+
+(defmacro simple-step-fct
+          "Build a simple step function for a transducer, based on
+          - a list of bindings (inner state)
+          - the body of a step function taking two arguments"
+          [[xf] bindings & step-fn]
+          `(fn [~xf]
+               (let ~bindings
+                    (fn
+                      ([] (~xf))
+                      ([result#] (~xf result#))
+                      ~@step-fn))))
 
