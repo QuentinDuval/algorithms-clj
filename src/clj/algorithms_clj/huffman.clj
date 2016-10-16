c2d0e70ec036d3df2b959ecdb183a30a6223a318
Duval Quentin <duvalquentin@macbook-pro-de-duval.home>
2016-10-16 20:52:55 +0200

    Fix the indentation level of intellij cursive

--- src/clj/algorithms_clj/huffman.clj
+++ src/clj/algorithms_clj/huffman.clj
@@ -1,10 +1,10 @@
 (ns algorithms-clj.huffman
-    (:require
-      [algorithms-clj.priority-map-utils :as prio-utils]
-      [algorithms-clj.utils :as utils]
-      [clojure.data.priority-map :as prio]
-      [clojure.set :as set]
-      ))
+  (:require
+    [algorithms-clj.priority-map-utils :as prio-utils]
+    [algorithms-clj.utils :as utils]
+    [clojure.data.priority-map :as prio]
+    [clojure.set :as set]
+    ))
 
 
 ;; -----------------------------------------------------------
@@ -26,34 +26,34 @@
 ;; -----------------------------------------------------------
 
 (defn- build-leaf-priority-queue
-       [value-frequency-pairs]
-       (into (prio/priority-map) (utils/map-first make-leaf) value-frequency-pairs))
+  [value-frequency-pairs]
+  (into (prio/priority-map) (utils/map-first make-leaf) value-frequency-pairs))
 
 (defn- merge-nodes-by-lowest-frequency
-       "Build the huffman tree from the priority map"
-       [heap]
-       (first
-         (utils/reduce-to-single-value [heap]
-                                       (let [[poped tail] (prio-utils/pop-n heap 2)
-                                             [a prio-a] (poped 0)
-                                             [b prio-b] (poped 1)]
-                                            (assoc tail (make-node a b) (+ prio-a prio-b)))
-                                       )))
+  "Build the huffman tree from the priority map"
+  [heap]
+  (first
+    (utils/reduce-to-single-value [heap]
+      (let [[poped tail] (prio-utils/pop-n heap 2)
+            [a prio-a] (poped 0)
+            [b prio-b] (poped 1)]
+        (assoc tail (make-node a b) (+ prio-a prio-b)))
+      )))
 
 (defn- adapt-root-leaf-tree
-       [huffman-tree]
-       (if (is-leaf? huffman-tree)
-         (make-node huffman-tree nil)
-         huffman-tree))
+  [huffman-tree]
+  (if (is-leaf? huffman-tree)
+    (make-node huffman-tree nil)
+    huffman-tree))
 
 (defn make-huffman-tree
-      "Build the huffman tree from a list of (value, frequence) pairs"
-      [value-frequency-pairs]
-      (->>
-        (build-leaf-priority-queue value-frequency-pairs)
-        (merge-nodes-by-lowest-frequency)
-        (adapt-root-leaf-tree)
-        ))
+  "Build the huffman tree from a list of (value, frequence) pairs"
+  [value-frequency-pairs]
+  (->>
+    (build-leaf-priority-queue value-frequency-pairs)
+    (merge-nodes-by-lowest-frequency)
+    (adapt-root-leaf-tree)
+    ))
 
 
 ;; -----------------------------------------------------------
@@ -61,41 +61,40 @@
 ;; -----------------------------------------------------------
 
 (defn huffman-tree->encoding-map
-      "Traverse the tree to get the symbol to code map"
-      [tree]
-      (defn go
-            [path node continue]                            ;; Use continuation to avoid recursion
-            (cond
-              (is-leaf? node) (continue [(leaf-val node) path])
-              (is-node? node)
-              (go (conj path 0) (child-at node 0)
-                  (fn [lhs]
-                      (if (child-at node 1)
-                        (go (conj path 1) (child-at node 1)
-                            (fn [rhs] (continue (concat lhs rhs))))
-                        (continue lhs))
-                      ))))
-      (apply hash-map (go [] tree identity)))
+  "Traverse the tree to get the symbol to code map"
+  [tree]
+  (defn go
+    [path node continue]                                    ;; Use continuation to avoid recursion
+    (cond
+      (is-leaf? node) (continue [(leaf-val node) path])
+      (is-node? node)
+      (go (conj path 0) (child-at node 0)
+        (fn [lhs]
+          (if (child-at node 1)
+            (go (conj path 1) (child-at node 1)
+              (fn [rhs] (continue (concat lhs rhs))))
+            (continue lhs))
+          ))))
+  (apply hash-map (go [] tree identity)))
 
 (defn encode-with
-      "Encode a stream of values with the decoder provided as first parameter"
-      [huffman-tree values]
-      (let [encoder (huffman-tree->encoding-map huffman-tree)]
-           (into [] (mapcat encoder) values)))
+  "Encode a stream of values with the decoder provided as first parameter"
+  [huffman-tree values]
+  (let [encoder (huffman-tree->encoding-map huffman-tree)]
+    (into [] (mapcat encoder) values)))
 
 (defn encode
-      "Encode a stream, using the stream frequences to build the huffman tree"
-      [values]
-      (let [t (-> values frequencies make-huffman-tree)]
-           [t (encode-with t values)]))
+  "Encode a stream, using the stream frequences to build the huffman tree"
+  [values]
+  (let [t (-> values frequencies make-huffman-tree)]
+    [t (encode-with t values)]))
 
 
 ;; -----------------------------------------------------------
 ;; Decoding
 ;; -----------------------------------------------------------
 
-(defn-
-  huffman-xf
+(defn- huffman-xf
   "Transducer step function to decode a huffman stream of values"
   [huffman-tree]
   (utils/make-simple-transducer
@@ -103,17 +102,17 @@
     [branch (volatile! huffman-tree)]
     ([result input]
       (let [next-branch (child-at @branch input)]
-           (if (is-leaf? next-branch)
-             (do
-               (vreset! branch huffman-tree)
-               (xf result (leaf-val next-branch)))
-             (do
-               (vreset! branch next-branch)
-               result))
-           ))))
+        (if (is-leaf? next-branch)
+          (do
+            (vreset! branch huffman-tree)
+            (xf result (leaf-val next-branch)))
+          (do
+            (vreset! branch next-branch)
+            result))
+        ))))
 
 (defn decode
-      "Decode a stream of inputs, provided the huffman tree as first parameter"
-      [huffman-tree inputs]
-      (into [] (huffman-xf huffman-tree) inputs))
+  "Decode a stream of inputs, provided the huffman tree as first parameter"
+  [huffman-tree inputs]
+  (into [] (huffman-xf huffman-tree) inputs))
 
